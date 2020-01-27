72ab8ec0a50763d2ed1a9eecb708c308316c622a
Josue <josue.durand8@gmail.com>
2020-01-26 20:22:39 -0500

    width

--- app/src/main/java/com/example/getscreenwidthandheigh/MainActivity.kt
+++ app/src/main/java/com/example/getscreenwidthandheigh/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.getscreenwidthandheigh
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // get device dimensions
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val width = displayMetrics.widthPixels
+        val height = displayMetrics.heightPixels
+
+        textWidth.text = "Width:  $width"
+        textHeight.text = "Height:  $height"
     }
 }
